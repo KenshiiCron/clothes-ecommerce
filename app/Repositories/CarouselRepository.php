37cabb7909b14bb556e18ce958ea00f3972d0dd8
islem-belhadef <islem.belhadef.dev@gmail.com>
2024-11-01 16:28:59 +0100

    added missing repos

---
+++ app/Repositories/CarouselRepository.php
@@ -0,0 +1,87 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Contracts\CarouselContract;
+use App\Models\Carousel;
+use app\Traits\UploadAble;
+use Illuminate\Support\Str;
+use JetBrains\PhpStorm\Pure;
+
+class CarouselRepository extends BaseRepositories implements CarouselContract
+{
+    use UploadAble;
+
+    /**
+     * @param Carousel $model
+     * @param array $filters
+     */
+    #[Pure]
+    public function __construct(Carousel $model, array $filters = [
+        \App\QueryFilter\Search::class,
+    ])
+    {
+        parent::__construct($model, $filters);
+    }
+
+
+    public function new(array $data)
+    {
+        if (array_key_exists('image', $data))
+        {
+            $data['image'] = $this->uploadOne($data['image'],(new \ReflectionClass($this->model))->getShortName().'/image');
+        }
+
+        activity()
+            ->causedBy(auth()->user())
+            ->performedOn($this->model)
+            ->event('store')
+            ->log('created: '. $this->model);
+
+        return $this->model::create($data);
+    }
+
+    public function update($model,array $data)
+    {
+        $model = $model instanceof $this->model ? $model : $this->findOneById($model);
+
+        if (array_key_exists('image',$data))
+        {
+            if ($model->image)
+            {
+                $this->deleteOne($model->image);
+            }
+            $data['image'] = $this->uploadOne($data['image'],(new ReflectionClass($this->model))->getShortName().'/image');
+        }
+
+        $model->update($data);
+
+        activity()
+            ->causedBy(auth()->user())
+            ->performedOn($this->model)
+            ->event('update')
+            ->log('edited: '. $this->model);
+
+        return $model->refresh();
+    }
+
+    public function destroy($model)
+    {
+        $model = $model instanceof $this->model ? $model : $this->findOneById($model);
+
+        if ($model->image)
+        {
+            $this->deleteOne($model->image);
+        }
+
+        activity()
+            ->causedBy(auth()->user())
+            ->performedOn($this->model)
+            ->event('destroy')
+            ->log('deleted: '. $this->model);
+
+        return $model ->delete();
+    }
+}
