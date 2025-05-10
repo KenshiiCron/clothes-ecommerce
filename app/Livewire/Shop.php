d60ffe4b30fa93d003f08dea6b4c97ca711c86d7
Skandar <skandarcron@gmail.com>
2025-05-10 16:57:15 +0000

    fixed pagination with filters

--- app/Livewire/Shop.php
+++ app/Livewire/Shop.php
@@ -13,7 +13,6 @@ use Livewire\WithPagination;
 
 class Shop extends Component
 {
-    use WithPagination;
     protected $products;
     protected $categories;
     public $attribute_values = [];
@@ -35,11 +34,10 @@ class Shop extends Component
         $categories = Category::whereHas('products')->get();
         $attributes = Attribute::whereHas('products')->get();
         $this->products = $product->findByFilter();
-        dd($this->products);
         return view('livewire.shop', ['categories' => $categories,'attributes' => $attributes, 'products' => $this->products]);
     }
 
-    public function updated($propertyName,ProductContract $product)
+    public function updated($propertyName)
     {
         if(str_contains($propertyName,'attribute_values'))
         {
